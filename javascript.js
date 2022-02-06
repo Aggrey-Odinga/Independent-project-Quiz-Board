88767b851ca1d1d8d5f58549669c796ecb5c7770
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-02-06 18:33:37 +0300

    feat(function): initialize function commands

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ const quizData = [{
 ];
 
 const quiz = document.getElementById('quiz')
-const answerE1s = document.querySelectorAll('answer')
+const answerE1s = document.querySelectorAll('.answer')
 const questionE1 = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
@@ -56,16 +56,52 @@ function loadQuiz() {
     const currentQuizData = quizData[currentQuiz]
 
     questionE1.innerText = currentQuiz.question
+    questionE1.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 
+console.log("qn", currentQuiz.question)
+
 }
 
 function deselectAnswers() {
-    answerE1s.forEach(answerE1s => answerE1s.checked = false)
+    answerE1s.forEach(answerE1 => answerE1.checked = false)
 }
 
+function getSelected() {
+    let answer
+    answerE1s.forEach(answerE1s =>{
+        if(answerE1s.checked){
+            answer = answerE1s.id
+        }
+    })
+    return answer
 
+    }
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct)
+        {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+            <h1>You answered ${score}/${quizData.length} questions correctly</h1>
+
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+            
+        
+    }
+})
 
