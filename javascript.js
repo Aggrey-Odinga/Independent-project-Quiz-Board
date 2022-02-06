0bf73ab8104b2c36e40b18531e7a9d577382bacd
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-02-06 16:39:56 +0300

    feat(javascript): add javascript questions

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const quizData = [
+
+        {
+        question: "JavaScript File Has An Extension of?",
+        a: ".java",
+        b: ".js",
+        c: ".javascript",
+        d: ".xml",
+        correct: "b",
+    },
+    {
+        
+    }
+]
